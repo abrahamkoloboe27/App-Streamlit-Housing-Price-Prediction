0a25620fba737fc9910baa05da99374e95cb4ce0
Abraham KOLOBOE <114146395+abrahamkoloboe27@users.noreply.github.com>
2024-11-21 21:09:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if st.sidebar.toggle("About the author"):
                         
                                         """)
 # Define the API endpoint
-API_ENDPOINT = st.text_input("API Endpoint", value="http://api:8000/predict")
+API_ENDPOINT = st.text_input("API Endpoint", value="https://abrahamklb-housing-api.hf.space/predict")
 
 # Define the Streamlit app
 def main():
