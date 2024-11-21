07a7c345d43a15ef8336c633ca5f173c6532325b
Abraham KOLOBOE <114146395+abrahamkoloboe27@users.noreply.github.com>
2024-11-21 21:48:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,75 +1,108 @@
 import streamlit as st
 import requests
 
-# Set the page configuration
+# Configurer la page
 st.set_page_config(
-        page_title="House Price Prediction",
-        page_icon=":house_with_garden:",
-        layout="wide",
-        initial_sidebar_state="expanded",
-    )
+    page_title="Prédiction du Prix des Maisons",
+    page_icon=":house_with_garden:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
-    # Add a title with an emoji
-st.title(":house_with_garden: House Price Prediction")
-if st.sidebar.toggle("About the author"):
-        with st.expander("Author", True) : 
-            c1, c2 = st.columns([1,2])
-            with c1 :
-                st.image("img/About the author.png")
-            with c2 : 
-                st.header(""" **S. Abraham Z. KOLOBOE**""")
-                st.markdown("""
-                    
-                    *:blue[Data Scientist | Ingénieur en Mathématiques et Modélisation]*
+# Ajouter du CSS personnalisé
+st.markdown("""
+    <style>
+    /* Style pour le fond de la page */
+    .main {
+        background-color: #f5f5f5;
+    }
+    /* Style pour la barre latérale */
+    .sidebar .sidebar-content {
+        background-color: #e6e6e6;
+    }
+    /* Style pour le bouton de soumission */
+    .stButton>button {
+        color: white;
+        background-color: #4CAF50;
+        padding: 0.5em 1em;
+        border-radius: 5px;
+    }
+    /* Style pour les en-têtes */
+    h1, h2, h3 {
+        color: #4CAF50;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
-                    Bonjour,
+# Ajouter un titre avec un emoji
+st.title(":house_with_garden: **Prédiction du Prix des Maisons**")
 
-                    Je suis Abraham KOLOBOE, un Data Scientist et Ingénieur en Mathématiques et Modélisation. 
-                    Mon expertise se situe dans les domaines des sciences de données et de l'intelligence artificielle. 
-                    Avec une approche technique et concise, je m'engage à fournir des solutions efficaces et précises dans mes projets.
-                            
-                    * Email : <abklb27@gmail.com>
-                    * WhatsApp : +229 91 83 84 21
-                    * Linkedin : [Abraham KOLOBOE](https://www.linkedin.com/in/abraham-zacharie-koloboe-data-science-ia-generative-llms-machine-learning)
+# Section de l'auteur dans la barre latérale
+if st.sidebar.checkbox("À propos de l'auteur"):
+    with st.expander("Auteur", True):
+        c1, c2 = st.columns([1, 2])
+        with c1:
+            st.image("img/About_the_author.png", width=150)
+        with c2:
+            st.header("**S. Abraham Z. KOLOBOE**")
+            st.markdown("""
+                *<span style='color:blue'>Data Scientist | Ingénieur en Mathématiques et Modélisation</span>*
+
+                Bonjour,
+
+                Je suis Abraham KOLOBOE, un Data Scientist et Ingénieur en Mathématiques et Modélisation. 
+                Mon expertise se situe dans les domaines des sciences de données et de l'intelligence artificielle. 
+                Avec une approche technique et concise, je m'engage à fournir des solutions efficaces et précises dans mes projets.
                         
-                                        """)
-# Define the API endpoint
-API_ENDPOINT = st.text_input("API Endpoint", value="https://abrahamklb-housing-api.hf.space/predict")
+                * **Email** : [abklb27@gmail.com](mailto:abklb27@gmail.com)
+                * **WhatsApp** : +229 91 83 84 21
+                * **LinkedIn** : [Abraham KOLOBOE](https://www.linkedin.com/in/abraham-zacharie-koloboe-data-science-ia-generative-llms-machine-learning)
+            """, unsafe_allow_html=True)
 
-# Define the Streamlit app
+# Définir le point de terminaison de l'API
+API_ENDPOINT = st.sidebar.text_input("Point de terminaison de l'API", value="https://abrahamklb-housing-api.hf.space/predict")
+
+# Définir l'application Streamlit
 def main():
-    # Create a sidebar toggle
+    # Ajouter des descriptions dans la barre latérale
     with st.sidebar:
-        if st.toggle("Show Project Description", True):
-            st.markdown("This project is a house price prediction application that uses a machine learning model to predict the price of a house based on various features such as area, number of bedrooms, bathrooms, etc.")
+        if st.checkbox("Afficher la description du projet", value=True):
+            st.markdown("""
+                ### Description du Projet
+                Ce projet est une application de prédiction des prix des maisons qui utilise un modèle d'apprentissage automatique pour prédire le prix d'une maison en fonction de diverses caractéristiques telles que la superficie, le nombre de chambres, de salles de bains, etc.
+            """)
 
-        if st.toggle("Show App Description",True):
-            st.markdown("This app allows users to input the details of a house and get a predicted price based on the machine learning model. Users can also modify the API endpoint to use a different model if desired.")
+        if st.checkbox("Afficher la description de l'application", value=True):
+            st.markdown("""
+                ### Description de l'Application
+                Cette application permet aux utilisateurs de saisir les détails d'une maison et d'obtenir un prix prédit basé sur le modèle d'apprentissage automatique. Les utilisateurs peuvent également modifier le point de terminaison de l'API pour utiliser un modèle différent si désiré.
+            """)
 
-    # Create a form
+    # Créer un formulaire
+    st.header("Entrez les détails de la maison")
     with st.form(key="house_form"):
-        # Create two columns
+        # Créer deux colonnes
         col1, col2 = st.columns(2)
 
-        # Create input fields in the first column
+        # Champs de saisie dans la première colonne
         with col1:
-            area = st.number_input("Area", value=2640)
-            bedrooms = st.number_input("Bedrooms", value=2)
-            bathrooms = st.number_input("Bathrooms", value=1)
-            stories = st.number_input("Stories", value=1)
-            mainroad = st.selectbox("Main Road", ["yes", "no"])
-            guestroom = st.selectbox("Guest Room", ["yes", "no"])
-            basement = st.selectbox("Basement", ["yes", "no"])
+            area = st.number_input("Superficie (en pieds carrés)", value=2640, min_value=0)
+            bedrooms = st.number_input("Chambres", value=2, min_value=0, step=1)
+            bathrooms = st.number_input("Salles de bains", value=1, min_value=0, step=1)
+            stories = st.number_input("Étages", value=1, min_value=0, step=1)
+            mainroad = st.selectbox("Route Principale", ["yes", "no"])
+            guestroom = st.selectbox("Chambre d'Amis", ["yes", "no"])
+            basement = st.selectbox("Sous-sol", ["yes", "no"])
 
-        # Create input fields in the second column
+        # Champs de saisie dans la deuxième colonne
         with col2:
-            hotwaterheating = st.selectbox("Hot Water Heating", ["yes", "no"])
-            airconditioning = st.selectbox("Air Conditioning", ["yes", "no"])
-            parking = st.number_input("Parking", value=1)
-            prefarea = st.selectbox("Preferred Area", ["yes", "no"])
-            furnishingstatus = st.selectbox("Furnishing Status", ["furnished", "unfurnished", "semi-furnished"])
+            hotwaterheating = st.selectbox("Chauffage à Eau Chaude", ["yes", "no"])
+            airconditioning = st.selectbox("Climatisation", ["yes", "no"])
+            parking = st.number_input("Parking", value=1, min_value=0, step=1)
+            prefarea = st.selectbox("Zone Préférée", ["yes", "no"])
+            furnishingstatus = st.selectbox("État d'Ameublement", ["furnished", "unfurnished", "semi-furnished"])
 
-        # Create a dictionary with the input data
+        # Créer un dictionnaire avec les données saisies
         data = {
             "area": area,
             "bedrooms": bedrooms,
@@ -85,14 +118,20 @@ def main():
             "furnishingstatus": furnishingstatus,
         }
 
-        # Create a submit button
-        submit_button = st.form_submit_button(label="Predict")
+        # Bouton de soumission
+        submit_button = st.form_submit_button(label="Prédire")
 
-    # Send a POST request to the API and display the predicted price
+    # Envoyer une requête POST à l'API et afficher le prix prédit
     if submit_button:
-        response = requests.post(API_ENDPOINT, json=data)
-        prediction = response.json()["prediction"]
-        st.metric(label="Predicted Price", value=f"{prediction:.3f}")
+        with st.spinner('Prédiction en cours...'):
+            try:
+                response = requests.post(API_ENDPOINT, json=data)
+                response.raise_for_status()
+                prediction = response.json()["prediction"]
+                # Afficher le prix prédit avec le symbole de l'euro
+                st.success(f"Le prix prédit est : **{prediction:,.2f} €**")
+            except requests.exceptions.RequestException as e:
+                st.error(f"Erreur lors de l'appel à l'API : {e}")
 
 if __name__ == "__main__":
     main()
